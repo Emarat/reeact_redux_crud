a5cc953a3767ca5d7e0b5bec8eccddc5ca1d6fa7
Emarat <emarathossain00@gmail.com>
2023-07-14 17:55:17 +0600

    feat: view books done

--- src/Components/ViewBook.jsx
+++ src/Components/ViewBook.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const ViewBook = () => {
+  const books = useSelector((state) => state.books);
+  console.log(books);
   return (
     <div>
-      <h2>View Book</h2>
+      <h2>List of Books</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books &&
+            books.map((book) => {
+              const { id, title, author } = book;
+              return (
+                <tr key={id}>
+                  <td>{id}</td>
+                  <td>{title}</td>
+                  <td>{author}</td>
+                  <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 };
